13bca6f65c60476c2b3c003fb64c09fa17b85f9f
srdothunter <srdothunter@gmail.com>
2021-03-28 22:58:39 +0800

    add links

--- js/main.js
+++ js/main.js
@@ -2,4 +2,8 @@ $(function () {
     $(".toggleLan").click(function () {
         $(".menu-area").toggle();
     })
+
+    $(".about").click(function () {
+        $('html, body').animate({scrollTop: $('#footer').offset().top}, 1000)
+    })
 })
